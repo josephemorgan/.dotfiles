4a5e3d452e20e1522099e61659d7446b79b2c79e
Joe <josephemorgan91@gmail.com>
2020-03-06 12:02:38 -0800

    Minor tweaks

--- emacs/.emacs.d/package-init.el
+++ emacs/.emacs.d/package-init.el
@@ -88,6 +88,7 @@
   (global-set-key (kbd "C-c a") 'org-agenda)
   (global-set-key (kbd "C-c c") 'org-capture)
   (add-hook 'org-mode-hook 'turn-on-flyspell)
+  (add-hook 'org-mode-hook 'visual-line-mode)
   )
 
 ;;; EVIL
