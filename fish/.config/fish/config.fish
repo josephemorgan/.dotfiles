db22ce90380a0bf5631599e3a2c829846c727f09
Joseph Morgan <joe@olorin.local>
2022-01-14 15:32:29 -0800

    Improved Fish configuration

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,1 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
 set fish_greeting
+
+switch (uname)
+	case Darwin
+		fish_add_path /opt/homebrew/bin/
+end
