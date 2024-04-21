cce3c45dd1d8b2516c657374e0f2c155f7c8bec1
Joseph Morgan <jm@jemorgan.dev>
2024-04-20 20:24:29 -0700

    Removing Neovim config to use submodule

--- nvim/.config/nvim/lua/keybindings.lua
+++
@@ -1,8 +0,0 @@
-
--- DAP Keybinds
---vim.keymap.set('n', '<F5>', function() require('dap').continue() end, {silent = true})
---vim.keymap.set('n', '<leader>dh', function() require('dap.ui.widgets').hover() end, {silent = true})
---vim.keymap.set('n', '<F9>', function() require('dap').toggle_breakpoint() end, {silent = true})
---vim.keymap.set('n', '<F10>', function() require('dap').step_over() end, {silent = true})
---vim.keymap.set('n', '<F11>', function() require('dap').step_into() end, {silent = true})
---vim.keymap.set('n', '<leader>dr', function() require('dap').repl.open() end, {silent = true})
