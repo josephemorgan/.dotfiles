fcc341a2f2ad4b89b86694c45d1b7bfdb27f70dd
Joseph Morgan <jm@jemorgan.dev>
2025-09-17 19:07:50 -0700

    Removing non-submodule neovim config

--- nvim/.config/nvim/lua/neovide.lua
+++
@@ -1,5 +0,0 @@
-return {
-	setup = function()
-		vim.o.guifont = "FiraCode Nerd Font:h15:e-subpixelantialias"
-	end,
-}
--- nvim/.config/nvim/lua/plugins/lualine.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	lazy = false,
-	opts = {
-		"filename",
-		path = 1,
-	},
-}
